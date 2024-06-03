e923c44958d07724f4dfbdf38854621bdb1140fc
dishit7 <hanmadishit74@gmail.com>
2024-06-03 07:18:48 +0530

    Add functionality of displaying all the categories and creating new categories

---
+++ frontend/src/components/Categories.jsx
@@ -0,0 +1,45 @@
+ import axios from "axios" 
+ import { useState,useEffect } from "react"
+ import {useNavigate} from "react-router-dom"
+ export const Categories =({onSelectCategory}) =>{
+    const [categories,setCategories]=useState([])
+    const navigate=useNavigate()
+
+
+    useEffect(()=>{
+        const result=axios.get("http://localhost:5050/category").then((response)=>{
+            setCategories(response.data)
+            console.log(categories)
+        })
+      },[])
+      
+      const handleAddCategory = ()=>{
+       navigate("/createcategory")
+      }
+      const handleCategoryClick = (category) => {
+        if (onSelectCategory) {
+          onSelectCategory(category);
+        }
+      };
+    
+
+return (<>
+     <div className="grid grid-cols-4">
+     <div className="bg-white text-black rounded-full w-12 h-12 text-center text-4xl" onClick={handleAddCategory}>+</div>
+        {categories.map((category)=>{
+            return(<div 
+            key={category.category_id}
+            onClick={() => handleCategoryClick(category)}
+            className="cursor-pointer"
+            >   {console.log(category.emoji+category.category_name)}
+                <div className="text-4xl"> {category.emoji}</div>
+                <div className="text-2xl"> {category.category_name}</div>
+
+                </div>
+            )
+        })}
+     </div>
+</>)
+
+     
+ }
---
+++ frontend/src/pages/CreateCategory.jsx
@@ -0,0 +1,53 @@
+import { Button } from "../ReusableComponents/Button";
+import axios from "axios"
+export const CreateCategory = () => {
+    async function createTag(e){
+        try{
+            console.log("hi")
+        e.preventDefault()
+        const formData=new FormData(e.target)
+        const emoji=formData.get("emoji")
+        const category_name=formData.get("category_name")
+
+        console.log(emoji+category_name)
+
+    const response=await axios.post("http://localhost:5050/category" ,{
+        emoji,
+        category_name
+    })
+    console.log(response.data)
+   alert("Catgory added")
+}catch(err){
+    alert("error occured pls try again")
+    console.log(err)
+}
+  }
+  return (
+    <>
+      <div className="bg-gray-900">
+        <div className="bg-black colour text-white sm:max-w-lg h-screen mx-auto flex flex-col justify-center items-center">
+          <div className="  border-t border-zinc-200 w-full">
+            <h1 className="text-gray-400 text-center mb-5 pt-3 ">NEW TAG</h1>
+            <form className="flex flex-col justify-center items-center gap-8 " onSubmit={createTag}>
+              <input
+                placeholder="(sticker)"
+                className="border-b outline-none text-center bg-transparent "
+                name="emoji"
+              />
+              <input
+                placeholder="(name)"
+                className="border-b outline-none text-center bg-transparent "
+                name="category_name"
+              />
+              <button
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                name="Add Category"
+                type="submit"
+              >Add Category</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
